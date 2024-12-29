60ef3151a7b8a7d5cadb60ffc4097ebf6506dc67
ArmanSardar <mdarmansardar57@gmail.com>
2024-12-29 12:15:24 +0600

    DreamHouseServerSide

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express')
+const app=express()
+const Houses=require('./HouseS.json')
+const cors=require('cors');
+const port =5000
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('this is DreamHOUse Home')
+})
+
+app.get('/houses',(req,res)=>{
+    res.send(Houses);
+})
+app.get('/houses/:id',(req,res)=>{
+    const id =parseInt( req.params.id)
+    const house=Houses.find(house=>house.id===id)
+    res.send(house)
+    console.log(house);
+})
+
+app.listen(port,()=>{
+    console.log(`this is running in port ${port} `);
+})
